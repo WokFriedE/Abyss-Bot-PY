2346fa8ad0c3a89c8b469388c96d1e1097c33b7f
WokFriedE <ethankh717@gmail.com>
2022-05-25 20:10:26 -0400

    Added Guild Lock

--- bot.py
+++ bot.py
@@ -6,6 +6,8 @@ from command_manager import commands
 
 load_dotenv(find_dotenv())
 TOKEN = os.getenv("TOKEN")
+GUILD = os.getenv("GUILD_ID")
+
 client = discord.Client()
 
 valid_channels = ['general', 'tst']
@@ -14,14 +16,18 @@ PREFIX = "!"
 
 @client.event
 async def on_ready():
-    print('{0.user} has connected to Discord'.format(client))
+    for guild in client.guilds:
+        if guild.name == GUILD:
+            break
+
+    print('{0.user} has connected to Discord '.format(client) + f'{guild.name}')
 
 
 @client.event
 async def on_message(message):
-    if message.author == client.user or not(message.channel.name in valid_channels) or message.content.startswith(PREFIX):
+    if message.author == client.user or not(message.channel.name in valid_channels) or not(message.content.startswith(PREFIX)):
         return
 
-    await commands(message)
+    await commands(message, PREFIX)
 
 client.run(TOKEN)
