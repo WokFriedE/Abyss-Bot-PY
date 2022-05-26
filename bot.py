b3529cac25679dc53576bc4912e1568957687f99
WokFriedE <ethankh717@gmail.com>
2022-05-25 23:21:53 -0400

    Added an add function

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ from colorama import Fore
 import discord
 import os
 from dotenv import load_dotenv, find_dotenv
-from command_manager import commands
+from command_manager import main
 
 load_dotenv(find_dotenv())
 TOKEN = os.getenv("TOKEN")
@@ -28,6 +28,6 @@ async def on_message(message):
     if message.author == client.user or not(message.channel.name in valid_channels) or not(message.content.startswith(PREFIX)):
         return
 
-    await commands(message, PREFIX)
+    await main(message, PREFIX)
 
 client.run(TOKEN)
