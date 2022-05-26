6c66f12fb5c4e40de1f849b01d25d5a65a8551e6
WokFriedE <ethankh717@gmail.com>
2022-05-25 20:10:38 -0400

    Added args ability

--- command_manager.py
+++ command_manager.py
@@ -1,19 +1,20 @@
 import discord
 
 
-async def commands(message):
+async def commands(message, PREFIX):
     username = str(message.author).split("#")[0]
     # turns jaja#0000 to [jaja, 0000] and then [jaja]
-    user_message = str(message.content)
+    user_command = str(message.content).split(" ")[0].strip(PREFIX)
+    args = str(message.content).split(" ")[1:]
     channel = str(message.channel)
-    print(f"{username} said: {user_message} in {channel}")
+    print(f"{username} said: {user_command} in {channel}")
 
-    if user_message.lower() == "ping":
+    if user_command.lower() == "ping" and len(args) == 0:
         # ways to return a reply to a message
         # await message.channel.send("pong", reference=message)
         await message.reply("pong")
         return
 
-    if user_message.lower() == "help":
+    if user_command.lower() == "help" and len(args) == 0:
         await message.channel.send("ping\nhelp")
         return
