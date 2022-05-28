7555f7a3715b99be59614cc1fb3d6e57e730c717
WokFriedE <ethankh717@gmail.com>
2022-05-28 02:28:24 -0400

    Added comments and new help

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+from turtle import title
+from unicodedata import name
 import discord  # requires "discord.py"
 from discord.ext import commands
 import os
@@ -14,9 +16,7 @@ TOKEN = os.getenv("TOKEN")
 GUILD = os.getenv("GUILD_ID")
 
 intents = discord.Intents.default()
-client = commands.Bot(command_prefix=',', intents=intents)
-
-valid_channels = ['general', 'tst']
+client = commands.Bot(command_prefix=',', intents=intents, help_command=None)
 
 
 @client.event
@@ -27,42 +27,67 @@ async def on_ready():
 
     print('{0.user} has connected to Discord '.format(client) + f'{guild.name}')
 
+#============================================================#
+# Utility Commands
+#============================================================#
 
-@client.command(name="credits")
+
+@client.command(name="credits", description="Displays the credits of the bot")
 async def credits(ctx):
     embed = discord.Embed(
         title="Credits", description="This bot was made by Actronav and WokFriedE to do some random commands!", url="https://github.com/WokFriedE/Abyss-Bot-PY", color=discord.Color.dark_gold())
     await ctx.send(embed=embed)
 
 
-@client.command()
+@client.command(name="help", aliases=["h"], description="Shows the commands of the bot")
+async def help(ctx):
+    helpText = ''
+    for command in client.commands:
+        helpText += f'{command.name}'
+        if(command.aliases != []):
+            helpText += f' ({", ".join(command.aliases)})'
+        helpText += f'\n{command.description}\n\n'
+    embed = discord.Embed(title="Help", description=helpText,
+                          color=discord.Color.dark_teal())
+    await ctx.send(embed=embed)
+
+#============================================================#
+# Random Commands
+#============================================================#
+
+
+@client.command(name="ping", description="Pings the bot")
 async def ping(ctx):
     await ctx.reply('pong')
 
 
-@client.command()
+@client.command(name="say", description="Says something the user enters")
 async def say(ctx, *arg):
     await ctx.send(" ".join(arg))
     await ctx.message.delete()
 
 
-@client.command()
+@client.command(name="add", description="Adds as many numbers together")
 async def add(ctx, *args):
     try:
-        args = [int(i) for i in args]
-        await ctx.send(f'The sum is {sum(args)}')
+        args = [float(i) for i in args]
+        await ctx.send(f'The sum is {round(sum(args),4)}')
     except:
         await ctx.send('Please enter numbers only')
 
 
-@client.command(name="test", aliases=['t'])
+@client.command(name="test", aliases=['t'], description="Just a test command")
 async def test(ctx):
     print(client.emojis)
     await client.loop.create_task(newEmoji(ctx, "test", "https://cdn.discordapp.com/emojis/431642989660471297.webp?size=80&quality=lossless"))
     message = await ctx.send("<:white_check_mark:979970509204770817>")
 
+#============================================================#
+# Emote Commands
+#============================================================#
 
-@client.command(name="newEmoji", aliases=['ne'])
+
+@client.command(name="newEmoji", aliases=['ne'], description="Adds a new emote to the server")
 async def newEmoji(ctx, name, item):
     response = requests.get(item)
     img = Image.open(BytesIO(response.content))
@@ -80,7 +105,7 @@ async def newEmoji(ctx, name, item):
         return
 
 
-@client.command(name="deleteEmoji", aliases=['e'])
+@client.command(name="deleteEmoji", aliases=['e'], description="Removes an emote on the server")
 async def deleteEmoji(ctx, emoji):
     if not ctx.author.guild_permissions.manage_emojis:
         await ctx.send('Invalid perms')
@@ -107,7 +132,7 @@ async def deleteEmoji(ctx, emoji):
         return
 
 
-@ client.command(name="getEmoji", aliases=['ge'])
+@ client.command(name="getEmoji", aliases=['ge'], description="Gets the URL of an emote")
 async def getEmoji(ctx, emoji: discord.Emoji):
     await ctx.send(emoji.url)
 
