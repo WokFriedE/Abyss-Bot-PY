4f94cf2822e1d436435b2b1e11ce58ec1dac7870
Alex Ha <haalex1112@gmail.com>
2022-05-27 23:19:10 -0400

    I hope this works

--- bot.py
+++ bot.py
@@ -2,13 +2,15 @@ import discord
 from discord.ext import commands
 import os
 from dotenv import load_dotenv, find_dotenv
+from github import Github
+import random
 
 load_dotenv(find_dotenv())
 TOKEN = os.getenv("TOKEN")
 GUILD = os.getenv("GUILD_ID")
 
 intents = discord.Intents.default()
-client = commands.Bot(command_prefix='!', intents=intents)
+client = commands.Bot(command_prefix='.', intents=intents)
 
 valid_channels = ['general', 'tst']
 
@@ -25,5 +27,35 @@ async def on_ready():
 @client.command()
 async def ping(ctx):
     await ctx.reply('pong')
+
+@client.command()
+async def test(ctx):
+    await ctx.reply("a")
+
+git = Github("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")
+aghpbRepo = git.get_repo("cat-milk/Anime-Girls-Holding-Programming-Books")
+list = ['AI', 'APL', 'ASM', 'Ada', 'Agda', 'Algorithms', 'Architecture', 'Beef', 'C#', 'C++', 'C', 'CSS', 'Cobol', 'Compilers', 'D', 'Dart', 'Delphi', 'Design Patterns', 'Editors', 'Elixir', 'Elm', 'F#', 'FORTH', 'Fortran', 'GDScript', 'Go', 'Haskell', 'HoTT', 'HolyC', 'Idris', 'Java', 'Javascript', 'Kotlin', 'Lisp', 'Lua', 'Math', 'Memes', 'Mixed', 'MongoDB', 'Nim', 'OCaml', 'Objective-C', 'Other', 'PHP', 'Perl', 'Personification', 'Prolog', 'Python', 'Quantum Computing', 'R', 'Racket', 'RayTracing', 'ReCT', 'Regex', 'Ruby', 'Rust', 'SICP', 'SQL', 'Scala', 'Shell', 'Smalltalk', 'Solidity', 'Swift', 'Systems', 'Typescript', 'Uncategorized', 'Unity', 'Unreal', 'V', 'VHDL', 'Verilog', 'Visual Basic', 'WebGL']
+
+@client.command(name="study",  aliases=['s'])
+async def study(ctx, *args):
+    if len(args) > 1:
+       await ctx.send("No more than one input allowed")
+    elif len(args) < 1:
+        fileSet = (aghpbRepo.get_contents(""))
+        randImageSet = (aghpbRepo.get_contents((fileSet[random.randrange(0, len(fileSet))]).name) )
+        await ctx.send(((randImageSet[random.randrange(0,len(randImageSet))]).html_url)  + "?raw=true")
+    else:
+        for x in list:
+           if x == args[0]:
+               imageSet = (aghpbRepo.get_contents(args[0]))
+               await ctx.send(((imageSet[random.randrange(0, len(imageSet))]).html_url) + "?raw=true")
+               return True
+
+        await ctx.send("Invalid study material, use .studymaterials or .sm to find valid ones")
+
+@client.command(name="studymaterials",  aliases=['sm'])
+async def studymaterials(ctx):
+    text = "```" + "\n".join(list) + "```"
+    await ctx.send(text)
 
 client.run(TOKEN)
