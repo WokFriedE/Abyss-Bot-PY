f09df1da8e62fe90cac081f902b45a3d5fe43ebc
WokFriedE <ethankh717@gmail.com>
2022-05-28 21:26:21 -0400

    Finalization to roles for polls and poll upgrade

--- bot.py
+++ bot.py
@@ -1,9 +1,3 @@
-from email import message
-from time import time
-from turtle import title
-from unicodedata import name
-from venv import create
-from webbrowser import get
 import discord  # requires "discord.py"
 from discord.ext import commands
 import os
@@ -110,9 +104,9 @@ async def test(ctx):
 
 git = Github(GITHUB_TOKEN)
 aghpbRepo = git.get_repo("cat-milk/Anime-Girls-Holding-Programming-Books")
-list = []
-list = [i.name for i in aghpbRepo.get_contents(
-    "/") if i.type == "dir" and i.name not in list and i.name != "Uncategorized"]
+gitList = []
+gitList = [i.name for i in aghpbRepo.get_contents(
+    "/") if i.type == "dir" and i.name not in gitList and i.name != "Uncategorized"]
 
 
 @client.command(name="study",  aliases=['s'], description="Gets a random image from the Github repo")
@@ -125,7 +119,7 @@ async def study(ctx, *args):
             (fileSet[random.randrange(0, len(fileSet))]).name))
         await ctx.send(((randImageSet[random.randrange(0, len(randImageSet))]).html_url) + "?raw=true")
     else:
-        for x in list:
+        for x in gitList:
             if x == args[0]:
                 imageSet = (aghpbRepo.get_contents(args[0]))
                 await ctx.send(((imageSet[random.randrange(0, len(imageSet))]).html_url) + "?raw=true")
@@ -136,7 +130,7 @@ async def study(ctx, *args):
 
 @client.command(name="studymaterials",  aliases=['sm'], description="Lists the \"study materials\"")
 async def studymaterials(ctx):
-    text = "```" + "\n".join(list) + "```"
+    text = "```" + "\n".join(gitList) + "```"
     await ctx.send(text)
 
 #============================================================#
@@ -162,24 +156,25 @@ async def newEmoji(ctx, name, item):
         return
 
 
-@client.command(name="deleteEmoji", aliases=['e'], description="Removes an emote on the server")
+@client.command(name="deleteEmoji", aliases=['de'], description="Removes an emote on the server")
 async def deleteEmoji(ctx, emoji):
-    # if (not ctx.author.guild_permissions.manage_emojis):
-    #     await ctx.send('Invalid perms')
-    #     return
 
     def check(reaction, user):
-        return user == ctx.author and str(reaction.emoji) == "✅" and reaction.message.id == msg.id
-
+        if(user == ctx.author and str(reaction.emoji) == "✅" and reaction.message.id == msg.id):
+            return True
+        elif (user == ctx.author and str(reaction.emoji) == "❌" and reaction.message.id == msg.id):
+            raise asyncio.TimeoutError("Cancelled")
     try:
         emoji = emoji.split(':')[2].strip('>')
         emojiDel = await ctx.guild.fetch_emoji(int(emoji))
         try:
             msg = await ctx.reply(embed=createEmbeded(title="Delete Emoji?", desc=f"Do you want to delete {emojiDel.name}", color=discord.Color.blurple(), image=emojiDel.url))
             await msg.add_reaction("✅")
-            await client.wait_for(event='reaction_add', timeout=60.0, check=check)
+            await msg.add_reaction("❌")
+            await client.wait_for(event='reaction_add', timeout=30.0, check=check)
         except asyncio.TimeoutError:
-            await ctx.send(embed=createEmbeded(title="Error", desc="Timeout", color=discord.Color.red()))
+            await ctx.send(embed=createEmbeded(title="Timeout", desc="Sorry but you ran out of time to delete this emoji", color=discord.Color.red(), image=emojiDel.url))
+            await msg.delete()
         else:
             await ctx.send(embed=createEmbeded(title="Deleted Emoji", desc="Deleted the emoji", color=discord.Color.green(), image=emojiDel.url))
             await msg.delete()
@@ -199,10 +194,14 @@ async def getEmoji(ctx, emoji):
     await ctx.send(embed=createEmbeded("Emoji", emoji.url, discord.Color.blue(), emoji.url))
 
 
-@client.command(name="pollNewEmoji", description="Creates a poll to add an emote\npollNewEmoji (time) (name) (emoji)", aliases=['pne'])
-async def pollNewEmoji(ctx, seconds="0", name="", emoji=""):
+@client.command(name="pollNewEmoji", description="Creates a poll to add an emote\npollNewEmoji (name) (emoji) (time)", aliases=['pne'])
+async def pollNewEmoji(ctx, name="", emoji="", seconds="20"):
     if seconds == 0:
         await ctx.reply('Please provide a time')
+        return
+
+    if len(list(filter(lambda role: role.name == "emojiRole", ctx.author.roles))) == 0:
+        await ctx.send('You do not have the required permissions')
         return
 
     votes = {"yes": [], "no": []}
@@ -212,11 +211,15 @@ async def pollNewEmoji(ctx, seconds="0", name="", emoji=""):
             return user == ctx.author and str(reaction.emoji) == "💣" and reaction.message.id == msg.id
         elif(user == ctx.author and str(reaction.emoji) == "🔚"):
             raise asyncio.TimeoutError("User ended poll")
-        elif(str(reaction.emoji) == "✅" and (user not in votes["yes"] or user not in votes["no"])):
+        elif(str(reaction.emoji) == "✅" and user not in votes["yes"]):
             votes["yes"].append(user)
+            if(user in votes["no"]):
+                votes["no"].remove(user)
             return False
-        elif(str(reaction.emoji) == "❌" and (user not in votes["yes"] or user not in votes["no"])):
+        elif(str(reaction.emoji) == "❌" and user not in votes["no"]):
             votes["no"].append(user)
+            if(user in votes["yes"]):
+                votes["yes"].remove(user)
             return False
 
     try:
@@ -251,10 +254,14 @@ async def pollNewEmoji(ctx, seconds="0", name="", emoji=""):
         return
 
 
-@client.command(name="pollDeleteEmoji", description="Creates a poll to delete an emote\npollDeleteEmoji (time) (emoji)", aliases=['pde'])
-async def pollNewEmoji(ctx, seconds="0", emoji=""):
+@client.command(name="pollDeleteEmoji", description="Creates a poll to delete an emote\npollDeleteEmoji (emoji) (time)", aliases=['pde'])
+async def pollNewEmoji(ctx, emoji="", seconds="20"):
     if seconds == 0:
         await ctx.reply('Please provide a time')
+        return
+
+    if len(list(filter(lambda role: role.name == "emojiRole", ctx.author.roles))) == 0:
+        await ctx.send('You do not have the required permissions')
         return
 
     votes = {"yes": [], "no": []}
@@ -264,11 +271,15 @@ async def pollNewEmoji(ctx, seconds="0", emoji=""):
             return user == ctx.author and str(reaction.emoji) == "💣" and reaction.message.id == msg.id
         elif(user == ctx.author and str(reaction.emoji) == "🔚"):
             raise asyncio.TimeoutError("User ended poll")
-        elif(str(reaction.emoji) == "✅" and (user not in votes["yes"] or user not in votes["no"])):
+        elif(str(reaction.emoji) == "✅" and user not in votes["yes"]):
             votes["yes"].append(user)
+            if(user in votes["no"]):
+                votes["no"].remove(user)
             return False
-        elif(str(reaction.emoji) == "❌" and (user not in votes["yes"] or user not in votes["no"])):
+        elif(str(reaction.emoji) == "❌" and user not in votes["no"]):
             votes["no"].append(user)
+            if(user in votes["yes"]):
+                votes["yes"].remove(user)
             return False
 
     try:
@@ -303,5 +314,87 @@ async def pollNewEmoji(ctx, seconds="0", emoji=""):
         await msg.edit(embed=createEmbeded(f"Remove the emoji '{emoji.name}'", f"Poll was exited for {emoji.name}", discord.Color.orange(), emoji))
         return
 
+#============================================================#
+# Role Commands
+#============================================================#
+
+
+@client.command(name="role", description="Creates a role\nrole (name)", aliases=['r'])
+async def role(ctx, name=""):
+    if name == "":
+        await ctx.send('Please provide a name')
+        return
+
+    ctx.guild.create_role(name=name)
+    await ctx.send(embed=createEmbeded(f"Created role '{name}'", f"{ctx.author.mention} created role '{name}'", discord.Color.green()))
+
+
+@client.command(name="roleEmojis", description="Gives someone the power to make emoji votes", aliases=['rE'])
+async def role(ctx, user):
+    if(not ctx.author.guild_permissions.manage_roles):
+        await ctx.reply('You do not have the perms to use this command')
+        return
+
+    try:
+        try:
+            user = await ctx.guild.fetch_member(int(user.strip('<@>')))
+        except:
+            await ctx.send('Invalid user')
+            return
+        else:
+            # adds role if doesnt exist
+            if("emojiRole" not in [role.name for role in ctx.guild.roles]):
+                await ctx.guild.create_role(name="emojiRole")
+
+            # Checks if user has the role
+            if(len(list(filter(lambda role: role.name == "emojiRole", user.roles))) == 0):
+                await user.add_roles([role for role in ctx.guild.roles if role.name == "emojiRole"][0])
+                await ctx.send(embed=createEmbeded(f"Gave {user.name} the perms for emojis", f"{ctx.author.mention} gave {user.mention} the power to make emoji votes", discord.Color.green()))
+            else:
+                await ctx.send(embed=createEmbeded(f"{user.name} already has the perms for emojis", f"{ctx.author.mention} tried to give {user.mention} the power to make emoji votes", discord.Color.red()))
+    except Exception as e:
+        await ctx.send(embed=createEmbeded(f"Failed to add {user.name} the perms for emojis", f"{ctx.author.mention} failed to add {user.mention} the power to make emoji votes", discord.Color.red()))
+        print(e)
+        return
+
+
+@client.command(name="roleRemoveEmojis", description="Removes the power to make emoji votes", aliases=['rRE'])
+async def role(ctx, user):
+    if(not ctx.author.guild_permissions.manage_roles):
+        await ctx.reply('You do not have the perms to use this command')
+        return
+
+    try:
+        try:
+            user = await ctx.guild.fetch_member(int(user.strip('<@>')))
+            if(len(list(filter(lambda role: role.name == "emojiRole", user.roles))) == 0):
+                await ctx.send(embed=createEmbeded(f"{user.name} does not have the role", f"{ctx.author.mention} tried to remove {user.mention} the power to make emoji votes", discord.Color.red()))
+                return
+        except:
+            ctx.send(embed=createEmbeded(f"Error Removing {user.name} the perms for emojis",
+                     f"{ctx.author.mention} failed to remove {user.mention} the power to make emoji votes", discord.Color.red()))
+            return
+        else:
+            await user.remove_roles([role for role in ctx.guild.roles if role.name == "emojiRole"][0])
+            await ctx.send(embed=createEmbeded(f"Removed {user.name} the perms for emojis", f"{ctx.author.mention} removed {user.mention} the power to make emoji votes", discord.Color.green()))
+    except:
+        await ctx.send(embed=createEmbeded(f"Failed to remove {user.name} the perms for emojis", f"{ctx.author.mention} failed to remove {user.mention} the power to make emoji votes", discord.Color.red()))
+        return
+
+
+@client.command(name="purgeRole", description="Purges a role", aliases=['pr'])
+async def purgeRole(ctx, roleDel):
+    if(not ctx.author.guild_permissions.manage_roles):
+        await ctx.reply('You do not have the perms to use this command')
+        return
+    try:
+        roleDel = ctx.guild.get_role(int(roleDel.strip('<@&>')))
+    except Exception as e:
+        await ctx.send(embed=createEmbeded(f"Failed to purge role", f"{ctx.author.mention} failed to purge role", discord.Color.red()))
+        print(e)
+        return
+    else:
+        await roleDel.delete()
+        await ctx.send(embed=createEmbeded(f"Purged role", f"{ctx.author.mention} purged role", discord.Color.green()))
 
 client.run(TOKEN)
