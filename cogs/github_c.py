90aedd76d09d7d1fd0cd785ed3c039bd29f5aea0
WokFriedE <ethankh717@gmail.com>
2022-05-31 01:55:12 -0400

    Touched up github commands for style

--- cogs/github_c.py
+++ cogs/github_c.py
@@ -18,31 +18,29 @@ class Github_Commands(commands.Cog):
             "/") if i.type == "dir" and i.name not in self.gitList and i.name != "Uncategorized"]
 
     @commands.command(name="study",  aliases=['s'], description="Gets a random image from the Github repo", usage="study\nstudy <category>")
-    async def study(self, ctx, *args):
-        if len(args) > 1:
-            await ctx.send("No more than one input allowed")
-        elif len(args) < 1:
+    async def study(self, ctx, catergory=""):
+        if catergory == "":
             fileSet = (self.aghpbRepo.get_contents(""))
             randImageSet = (self.aghpbRepo.get_contents(
                 (fileSet[random.randrange(0, len(fileSet))]).name))
             image_url = (
                 (randImageSet[random.randrange(0, len(randImageSet))]).html_url)
             await ctx.send(embed=createEmbeded(title="Random Image", desc="", color=discord.Color.blue(), image=(image_url + "?raw=true"), url=image_url))
+            return
         else:
-            for x in self.gitList:
-                if x == args[0]:
-                    imageSet = (self.aghpbRepo.get_contents(args[0]))
-                    image_url = (
-                        (imageSet[random.randrange(0, len(imageSet))]).html_url)
-                    await ctx.send(embed=createEmbeded(title="Random Image", desc=f"from {args[0]}", color=discord.Color.blue(), image=(image_url + "?raw=true"), url=image_url))
-                    return True
+            if(catergory in self.gitList):
+                imageSet = (self.aghpbRepo.get_contents(catergory))
+                image_url = (
+                    (imageSet[random.randrange(0, len(imageSet))]).html_url)
+                await ctx.send(embed=createEmbeded(title=f"Random Image from {catergory}", color=discord.Color.blue(), image=(image_url + "?raw=true"), url=image_url))
+                return
 
-            await ctx.send("Invalid study material, use .studymaterials or .sm to find valid ones")
+        await ctx.send("Invalid study material, use .studymaterials or .sm to find valid ones")
 
     @commands.command(name="studymaterials",  aliases=['sm'], description="Lists the \"study materials\"", usage="studymaterials")
     async def studymaterials(self, ctx):
-        text = "```" + "\n".join(self.gitList) + "```"
-        await ctx.send(text)
+        text = "\n".join(self.gitList)
+        await ctx.send(embed=createEmbeded(title="Study Materials", desc=text, color=discord.Color.blue()))
 
 
 def setup(client):
